803525b005929fff429307e540d61bf1eb3d6439
Trophy198 <spsp8426@naver.com>
2024-04-13 15:22:52 +0900

    design: add mainpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from '@/styles/Home.module.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
+    <div className={styles.container}>
+      <h1>The main page is being prepared</h1>
     </div>
   );
 }
