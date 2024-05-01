b9b517af899794734a21bd6793dbc053e30394c1
KanghoYoo <kangsick98@gmail.com>
2024-05-01 21:22:45 +0900

    add : create footer component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import styles from '@/styles/Footer.module.css';
+import GitHubIcon from './../../public/icons/github.png';
+import DiscordIcon from './../../public/icons/discord.png';
+
+const Footer = () => {
+  const gitHubURL: string = 'https://github.com/alexemanuelol/rustplusplus';
+  const discordURL: string = 'https://discord.com/invite/vcrKbKVAbc';
+  const privacyPolicyURL: string =
+    'https://sites.google.com/view/rustplusplus-privacy-policy/%ED%99%88';
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerImage}>
+        <a href={gitHubURL} target="_blank">
+          <Image
+            src={GitHubIcon}
+            alt="github install link"
+            placeholder="blur"
+            width={45}
+            height={45}
+          />
+        </a>
+        <a href={discordURL} target="_blank">
+          <Image
+            src={DiscordIcon}
+            alt="discord community link"
+            placeholder="blur"
+            width={45}
+            height={45}
+          />
+        </a>
+      </div>
+      <div className={styles.footerText}>
+        <p>
+          <a href={privacyPolicyURL} target="_blank">
+            Privacy Policy
+          </a>
+        </p>
+        <p>Copyright ©2024 RUSTPLUSPLUS Inc. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
