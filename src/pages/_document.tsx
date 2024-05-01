b6c47897fd46432aaa822d4ed17c90795e6330d4
Trophy198 <spsp8426@naver.com>
2024-05-01 21:45:46 +0900

    feat : add meta tags in _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
