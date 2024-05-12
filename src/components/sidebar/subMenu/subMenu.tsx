4a02056f7e811abfa90f5b5f1f4a3586a4c3a577
KanghoYoo <kangsick98@gmail.com>
2024-05-12 14:45:29 +0900

    feat : Enable sidebar close on mobile when navigating to different lists, remove empty tags

--- src/components/sidebar/subMenu/subMenu.tsx
+++ src/components/sidebar/subMenu/subMenu.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styles from './subMenu.module.css';
 import { Props } from '@/types/sidebarTypes';
 import { usePathname } from 'next/navigation';
+import useSidebarStore from '@/store/useSidebarStore';
 
 const SubMenu = ({ items }: Props) => {
   const path = usePathname();
@@ -10,41 +11,34 @@ const SubMenu = ({ items }: Props) => {
     return path.endsWith(itemPath);
   };
 
+  const { toggleSidebar } = useSidebarStore();
+
   return (
-    <>
-      <ul className={styles.container}>
-        <li className={styles.menuItems}>
-          <Link
-            href={items.href}
-            className={`${styles.header} ${
-              isSelected(items.href) ? styles.selected : ''
-            }`}
-          >
-            {items.label}
-          </Link>
-          {items.subItems && (
-            <div className={styles.subMenu}>
-              <div className={styles.subMenuItem}>
-                <ul className={styles.subMenuItems}>
-                  {items.subItems.map((subItem) => (
-                    <li key={subItem.id} className={styles.subItem}>
-                      <Link
-                        href={subItem.href}
-                        className={`${styles.links} ${
-                          isSelected(subItem.href) ? styles.selected : ''
-                        }`}
-                      >
-                        {subItem.label}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+    <ul className={styles.container} onClick={toggleSidebar}>
+      <li className={styles.menuItems}>
+        <Link href={items.href} className={`${styles.header} ${isSelected(items.href) ? styles.selected : ''}`}>
+          {items.label}
+        </Link>
+        {items.subItems && (
+          <div className={styles.subMenu}>
+            <div className={styles.subMenuItem}>
+              <ul className={styles.subMenuItems}>
+                {items.subItems.map((subItem) => (
+                  <li key={subItem.id} className={styles.subItem}>
+                    <Link
+                      href={subItem.href}
+                      className={`${styles.links} ${isSelected(subItem.href) ? styles.selected : ''}`}
+                    >
+                      {subItem.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
-          )}
-        </li>
-      </ul>
-    </>
+          </div>
+        )}
+      </li>
+    </ul>
   );
 };
 
