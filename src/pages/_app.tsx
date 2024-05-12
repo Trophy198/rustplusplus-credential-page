c9406e4007ee48102d10b8ea18d6b70a9584df5e
Trophy198 <spsp8426@naver.com>
2024-05-13 00:37:45 +0900

    feat : add meta tag for SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,12 +15,32 @@ export default function App({ Component, pageProps }: AppProps) {
     fetchLoginStatus();
   }, [fetchLoginStatus]);
 
+  const siteUrl = 'https://rustplusplus-credentials.netlify.app';
+  const logoUrl = `${siteUrl}/images/rustplusplusLogo.png`;
+
   return (
     <>
       <Head>
         <title>rustplusplus</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="rustPlusPlus Page" />
+
+        <meta property="og:title" content="rustPlusPlus" />
+        <meta
+          property="og:description"
+          content="Enhance your Rust experience with rustPlusPlus. Get maps, server information, event details, and more."
+        />
+        <meta property="og:image" content={logoUrl} />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="rustPlusPlus" />
+        <meta
+          name="twitter:description"
+          content="Enhance your Rust experience with rustPlusPlus. Get maps, server information, event details, and more."
+        />
+        <meta name="twitter:image" content={logoUrl} />
       </Head>
       {noHeader ? (
         <Component {...pageProps} />
