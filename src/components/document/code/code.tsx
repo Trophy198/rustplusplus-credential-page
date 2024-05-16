9e69dfdb1152c3187d2041de7f41616c342336ba
Trophy198 <spsp8426@naver.com>
2024-05-16 23:19:57 +0900

    refactor : restructure Code component

--- src/components/document/code/code.tsx
+++ src/components/document/code/code.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import styles from './code.module.css';
 
-const Code = ({ children }: { children: ReactNode }) => <code className={styles.highlightCode}>{children}</code>;
+const Code = ({ children }: { children: ReactNode }) => {
+  return <code className={styles.highlightCode}>{children}</code>;
+};
 
 export default Code;
