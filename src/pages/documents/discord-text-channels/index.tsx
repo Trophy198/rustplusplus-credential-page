a8cb606955780b22acc74e768ea6b70e0c33f971
Trophy198 <spsp8426@naver.com>
2024-05-16 17:27:29 +0900

    feat : apply SSG to all pages including /documents

--- src/pages/documents/discord-text-channels/index.tsx
+++ src/pages/documents/discord-text-channels/index.tsx
@@ -1,18 +1,27 @@
-import Layout from '@/components/document/layout';
+import DocumentLayout from '@/components/document/layout';
+import Pagination from '@/components/pagination/pagination';
 import menuItems from '@/config/menuItems';
+import usePagination from '@/hooks/usePagination';
 import { MenuRender } from '@/types/sidebarTypes';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
-export const getServerSideProps: GetServerSideProps<MenuRender> = async () => {
+const DiscordTextChannelsPage = ({ menuItems }: MenuRender) => {
+  const { previousHref, previousLabel, nextHref, nextLabel } = usePagination();
+
+  return (
+    <DocumentLayout menuItems={menuItems}>
+      <h1>Disocrd Text Channels</h1>
+      <Pagination previousHref={previousHref} previousLabel={previousLabel} nextHref={nextHref} nextLabel={nextLabel} />
+    </DocumentLayout>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       menuItems,
     },
   };
-};
-
-const DiscordTextChannelsPage = ({ menuItems }: MenuRender) => {
-  return <Layout menuItems={menuItems}>DiscordTextChannelsPage !!</Layout>;
 };
 
 export default DiscordTextChannelsPage;
