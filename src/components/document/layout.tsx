0973f7a29d3bac2a2eb425163a928f2038bfef5b
KanghoYoo <kangsick98@gmail.com>
2024-05-17 16:36:22 +0900

    feat : Functions to detect layout size and prevent body scroll

--- src/components/document/layout.tsx
+++ src/components/document/layout.tsx
@@ -1,8 +1,9 @@
 import Sidebar from '@/components/sidebar/sidebar';
 import styles from './layout.module.css';
 import { MenuRender } from '@/types/sidebarTypes';
-import { ReactNode } from 'react';
+import { ReactNode, useLayoutEffect } from 'react';
 import SidebarMenu from '../sidebarMenu/sidebarMenu';
+import useSidebarStore from '@/store/useSidebarStore';
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,6 +11,33 @@ interface LayoutProps {
 }
 
 const DocumentLayout = ({ children, menuItems }: LayoutProps) => {
+  const { isSidebarOpen, setSidebarClosed } = useSidebarStore();
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setSidebarClosed();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setSidebarClosed]);
+
+  useLayoutEffect(() => {
+    if (isSidebarOpen) {
+      document.body.style.overflow = 'hidden';
+      document.body.style.touchAction = 'none';
+    } else {
+      document.body.style.overflow = '';
+      document.body.style.touchAction = '';
+    }
+  }, [isSidebarOpen]);
+
   return (
     <div className={styles.container}>
       <SidebarMenu />
