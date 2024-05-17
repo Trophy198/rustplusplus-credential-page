8957b2731c39af3d8085845fcb0f877b19157492
KanghoYoo <kangsick98@gmail.com>
2024-05-17 16:23:49 +0900

    add: Function to close scrollbar on screen resize

--- src/store/useSidebarStore.ts
+++ src/store/useSidebarStore.ts
@@ -3,11 +3,13 @@ import { create } from 'zustand';
 interface SidebarState {
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
+  setSidebarClosed: () => void;
 }
 
 const useSidebarStore = create<SidebarState>((set) => ({
   isSidebarOpen: false,
   toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+  setSidebarClosed: () => set({ isSidebarOpen: false }),
 }));
 
 export default useSidebarStore;
