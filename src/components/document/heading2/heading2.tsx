87c921b36769a7eff2d5658372ca9ef8c0f6a70e
Trophy198 <spsp8426@naver.com>
2024-05-20 01:32:38 +0900

    feat : add id prop to Heading2 component

--- src/components/document/heading2/heading2.tsx
+++ src/components/document/heading2/heading2.tsx
@@ -1,8 +1,12 @@
 import { ReactNode } from 'react';
 import styles from './heading2.module.css';
 
-const Heading2 = ({ children }: { children: ReactNode }) => {
-  return <h2 className={styles.heading2}>{children}</h2>;
+const Heading2 = ({ id, children }: { id?: string; children: ReactNode }) => {
+  return (
+    <h2 className={styles.heading2} id={id}>
+      {children}
+    </h2>
+  );
 };
 
 export default Heading2;
