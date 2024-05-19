e582210d333d50af4160bfdc6bac8340142dd8a1
eunsung <esk114477@gmail.com>
2024-05-19 18:18:09 +0900

    refactor: fix type TableComponentTypes

--- src/types/tableComponentTypes.ts
+++ src/types/tableComponentTypes.ts
@@ -1,14 +1,9 @@
-export interface TableColumnType {
-  key: string;
-  label: string;
-}
-
-export interface TableRowType {
+export interface TableType {
   key: string;
   [key: string]: string;
 }
 
 export interface CommonTableProps {
-  columns: TableColumnType[];
-  rows: TableRowType[];
+  columns: TableType[];
+  rows: TableType[];
 }
