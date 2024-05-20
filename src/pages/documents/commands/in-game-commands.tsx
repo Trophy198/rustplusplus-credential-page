6a97d9dd73d07abdf84fe42ad65d567935e87184
Trophy198 <spsp8426@naver.com>
2024-05-21 00:04:21 +0900

    feat: add image URLs to SSG props in in-game page

--- src/pages/documents/commands/in-game-commands.tsx
+++ src/pages/documents/commands/in-game-commands.tsx
@@ -1,16 +1,17 @@
 import DocumentLayout from '@/components/document/layout';
+import InGameCommandsSection from '@/components/document/pages/in-game-commands/section';
 import Pagination from '@/components/pagination/pagination';
 import menuItems from '@/config/menuItems';
+import { ImageUrls } from '@/data/documents/inGameCommands/images';
 import usePagination from '@/hooks/usePagination';
-import { MenuRender } from '@/types/sidebarTypes';
-import { GetStaticProps } from 'next';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-const InGameCommandsPage = ({ menuItems }: MenuRender) => {
+const InGameCommandsPage = ({ menuItems, ImageUrls }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { previousHref, previousLabel, nextHref, nextLabel } = usePagination();
 
   return (
     <DocumentLayout menuItems={menuItems}>
-      <h1>In-Game Commands</h1>
+      <InGameCommandsSection imageUrls={ImageUrls} />
       <Pagination previousHref={previousHref} previousLabel={previousLabel} nextHref={nextHref} nextLabel={nextLabel} />
     </DocumentLayout>
   );
@@ -20,6 +21,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       menuItems,
+      ImageUrls,
     },
   };
 };
