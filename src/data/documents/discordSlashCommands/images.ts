e0ee26cbc42ea9a72f8798be940ab0e92ed3c29b
eunsung <esk114477@gmail.com>
2024-05-20 20:07:36 +0900

    feat: add images.ts

---
+++ src/data/documents/discordSlashCommands/images.ts
@@ -0,0 +1,51 @@
+import AlarmsSlashCommand from '@/public/images/discordSlashCommands/alarms_edit.png';
+import AliasSlashCommand from '@/public/images/discordSlashCommands/alias.png';
+import BlacklistSlashCommand from '@/public/images/discordSlashCommands/blacklist.png';
+import CctvSlashCommand from '@/public/images/discordSlashCommands/cctv.png';
+import CraftSlashCommand from '@/public/images/discordSlashCommands/craft.png';
+import CredentialSlashCommand from '@/public/images/discordSlashCommands/credentials.png';
+import DecaySlashCommand from '@/public/images/discordSlashCommands/decay.png';
+import HelpSlashCommand from '@/public/images/discordSlashCommands/help.png';
+import ItemSlashCommand from '@/public/images/discordSlashCommands/item.png';
+import LeaderSlashCommand from '@/public/images/discordSlashCommands/leader.png';
+import MapSlashCommand from '@/public/images/discordSlashCommands/map.png';
+import MarketSlashCommand from '@/public/images/discordSlashCommands/market.png';
+import Players_all_players from '@/public/images/discordSlashCommands/players_all_players.png';
+import Players_specific_user from '@/public/images/discordSlashCommands/players_specific_user.png';
+import PlayersSlashCommand from '@/public/images/discordSlashCommands/players.png';
+import RecycleSlashCommand from '@/public/images/discordSlashCommands/recycle.png';
+import ResearchSlashCommand from '@/public/images/discordSlashCommands/research.png';
+import ResetSlashCommand from '@/public/images/discordSlashCommands/reset.png';
+import RoleSlashCommand from '@/public/images/discordSlashCommands/role.png';
+import StoragemonitorSlashCommand from '@/public/images/discordSlashCommands/storagemonitor.png';
+import SwitchSlashCommand from '@/public/images/discordSlashCommands/switch.png';
+import UpkeepSlashCommand from '@/public/images/discordSlashCommands/upkeep.png';
+import UptimeSlashCommand from '@/public/images/discordSlashCommands/uptime.png';
+import VoiceSlashCommand from '@/public/images/discordSlashCommands/voice.png';
+
+export const ImageUrls = {
+  AlarmsSlashCommand,
+  AliasSlashCommand,
+  BlacklistSlashCommand,
+  CctvSlashCommand,
+  CraftSlashCommand,
+  CredentialSlashCommand,
+  DecaySlashCommand,
+  HelpSlashCommand,
+  ItemSlashCommand,
+  LeaderSlashCommand,
+  MapSlashCommand,
+  MarketSlashCommand,
+  PlayersSlashCommand,
+  Players_all_players,
+  Players_specific_user,
+  RecycleSlashCommand,
+  ResearchSlashCommand,
+  ResetSlashCommand,
+  RoleSlashCommand,
+  StoragemonitorSlashCommand,
+  SwitchSlashCommand,
+  UpkeepSlashCommand,
+  UptimeSlashCommand,
+  VoiceSlashCommand,
+};
