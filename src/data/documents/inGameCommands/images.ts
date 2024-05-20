01cdb88c3ed9d651da562e0b6eb3d4339720973d
Trophy198 <spsp8426@naver.com>
2024-05-21 00:03:48 +0900

    feat: add image data and command list for in-game commands

---
+++ src/data/documents/inGameCommands/images.ts
@@ -0,0 +1,79 @@
+import AfkIngameCommand from '@/public/images/inGameCommands/afk_ingame.png';
+import AliveIngameCommand from '@/public/images/inGameCommands/alive_ingame.png';
+import CargoIngameCommand from '@/public/images/inGameCommands/cargo_ingame.png';
+import ChinookIngameCommand from '@/public/images/inGameCommands/chinook_ingame.png';
+import ConnectionIngameCommand from '@/public/images/inGameCommands/connection_ingame.png';
+import CraftIngameCommand from '@/public/images/inGameCommands/craft_ingame.png';
+import DeathIngameCommand from '@/public/images/inGameCommands/death_ingame.png';
+import DecayIngameCommand from '@/public/images/inGameCommands/decay_ingame.png';
+import EventsIngameCommand from '@/public/images/inGameCommands/events_ingame.png';
+import HeliIngameCommand from '@/public/images/inGameCommands/heli_ingame.png';
+import LargeIngameCommand from '@/public/images/inGameCommands/large_ingame.png';
+import LeaderIngameCommand from '@/public/images/inGameCommands/leader_ingame.png';
+import MarkerIngameCommand from '@/public/images/inGameCommands/marker_ingame.png';
+import MarketIngameCommand from '@/public/images/inGameCommands/market_ingame.png';
+import MuteIngameCommand from '@/public/images/inGameCommands/mute_ingame.png';
+import NotesIngameCommand from '@/public/images/inGameCommands/notes_ingame.png';
+import OfflineIngameCommand from '@/public/images/inGameCommands/offline_ingame.png';
+import OnlineIngameCommand from '@/public/images/inGameCommands/online_ingame.png';
+import PlayerIngameCommand from '@/public/images/inGameCommands/player_ingame.png';
+import PlayersIngameCommand from '@/public/images/inGameCommands/players_ingame.png';
+import PopIngameCommand from '@/public/images/inGameCommands/pop_ingame.png';
+import ProxIngameCommand from '@/public/images/inGameCommands/prox_ingame.png';
+import RecycleIngameCommand from '@/public/images/inGameCommands/recycle_ingame.png';
+import ResearchIngameCommand from '@/public/images/inGameCommands/research_ingame.png';
+import SendIngameCommand from '@/public/images/inGameCommands/send_ingame.png';
+import SmallIngameCommand from '@/public/images/inGameCommands/small_ingame.png';
+import SteamidIngameCommand from '@/public/images/inGameCommands/steamid_ingame.png';
+import TeamIngameCommand from '@/public/images/inGameCommands/team_ingame.png';
+import TimeIngameCommand from '@/public/images/inGameCommands/time_ingame.png';
+import TimerIngameCommand from '@/public/images/inGameCommands/timer_ingame.png';
+import LanguageCodeIngameCommand from '@/public/images/inGameCommands/language_code_ingame.png';
+import TranslateToIngameCommand from '@/public/images/inGameCommands/translateTo_ingame.png';
+import TranslateFromIngameCommand from '@/public/images/inGameCommands/translateFrom_ingame.png';
+import TtsIngameCommand from '@/public/images/inGameCommands/tts_ingame.png';
+import UnmuteIngameCommand from '@/public/images/inGameCommands/unmute_ingame.png';
+import UpKeepIngameCommand from '@/public/images/inGameCommands/upkeep_ingame.png';
+import UptimeIngameCommand from '@/public/images/inGameCommands/uptime_ingame.png';
+import WipeIngameCommand from '@/public/images/inGameCommands/wipe_ingame.png';
+
+export const ImageUrls = {
+  AfkIngameCommand,
+  AliveIngameCommand,
+  CargoIngameCommand,
+  ChinookIngameCommand,
+  ConnectionIngameCommand,
+  CraftIngameCommand,
+  DeathIngameCommand,
+  DecayIngameCommand,
+  EventsIngameCommand,
+  HeliIngameCommand,
+  LargeIngameCommand,
+  LeaderIngameCommand,
+  MarkerIngameCommand,
+  MarketIngameCommand,
+  MuteIngameCommand,
+  NotesIngameCommand,
+  OfflineIngameCommand,
+  OnlineIngameCommand,
+  PlayerIngameCommand,
+  PlayersIngameCommand,
+  PopIngameCommand,
+  ProxIngameCommand,
+  RecycleIngameCommand,
+  ResearchIngameCommand,
+  SendIngameCommand,
+  SmallIngameCommand,
+  SteamidIngameCommand,
+  TeamIngameCommand,
+  TimeIngameCommand,
+  TimerIngameCommand,
+  LanguageCodeIngameCommand,
+  TranslateToIngameCommand,
+  TranslateFromIngameCommand,
+  TtsIngameCommand,
+  UnmuteIngameCommand,
+  UpKeepIngameCommand,
+  UptimeIngameCommand,
+  WipeIngameCommand,
+};
