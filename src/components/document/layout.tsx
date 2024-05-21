03c62115f227e7318df92b3fe240ea410498808c
KanghoYoo <kangsick98@gmail.com>
2024-05-21 18:23:45 +0900

    remove : Remove redundant code from common layout and utilize modal background component

--- src/components/document/layout.tsx
+++ src/components/document/layout.tsx
@@ -1,10 +1,8 @@
 import Sidebar from '@/components/sidebar/sidebar';
 import styles from './layout.module.css';
 import { MenuRender } from '@/types/sidebarTypes';
-import { ReactNode, useLayoutEffect } from 'react';
-import SidebarMenu from '../sidebarMenu/sidebarMenu';
-import useSidebarStore from '@/store/useSidebarStore';
-import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+import ModalBackground from '@/components/modalBackground/modalBackground';
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,45 +10,9 @@ interface LayoutProps {
 }
 
 const DocumentLayout = ({ children, menuItems }: LayoutProps) => {
-  const { isSidebarOpen, setSidebarClosed } = useSidebarStore();
-  const router = useRouter();
-
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setSidebarClosed();
-      }
-    };
-
-    const handleRouteChange = () => {
-      setSidebarClosed();
-      document.body.style.overflow = '';
-      document.body.style.touchAction = '';
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    router.events.on('routeChangeStart', handleRouteChange);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, [router.events, setSidebarClosed]);
-
-  useLayoutEffect(() => {
-    if (isSidebarOpen) {
-      document.body.style.overflow = 'hidden';
-      document.body.style.touchAction = 'none';
-    } else {
-      document.body.style.overflow = '';
-      document.body.style.touchAction = '';
-    }
-  }, [isSidebarOpen]);
-
   return (
     <div className={styles.container}>
-      <SidebarMenu />
+      <ModalBackground />
       <Sidebar menuItems={menuItems} />
       <section>{children}</section>
     </div>
