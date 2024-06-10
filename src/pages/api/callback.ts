d72470299d3f5287d4a4cb2ae016154b496c0bed
Trophy198 <spsp8426@naver.com>
2024-06-10 22:08:38 +0900

    refactor : update callback API - apply push-receiver-v2 - use env.local file

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
-const { register }: { register: any } = require('push-receiver');
+const { register }: { register: any } = require('push-receiver-v2');
 
 interface FCMCredentials {
   fcm: {
@@ -10,13 +10,17 @@ interface FCMCredentials {
   };
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const firebaseConfig = {
+    firebase: {
+      apiKey: process.env.API_KEY,
+      appID: process.env.APP_ID,
+      projectID: process.env.PROJECT_ID,
+    },
+  };
   try {
     const deviceId = uuidv4();
-    const fcmCredentials: FCMCredentials = await register('976529667804');
+    const fcmCredentials: FCMCredentials = await register(firebaseConfig);
     const fcmToken = fcmCredentials.fcm.token;
     const expoPushToken = await getExpoPushToken(deviceId, fcmToken);
     const { token, steamId } = req.query;
@@ -27,11 +31,7 @@ export default async function handler(
       return;
     }
 
-    const registrationData = await registerWithRustPlus(
-      token,
-      deviceId,
-      expoPushToken,
-    );
+    const registrationData = await registerWithRustPlus(token, deviceId, expoPushToken);
     const rustplusToken = registrationData.token;
     const header = decodeJWTHeader(rustplusToken);
     const expiryDate = new Date(header.exp * 1000);
@@ -43,51 +43,34 @@ export default async function handler(
     };
 
     res.setHeader('Set-Cookie', [
-      `config=${encodeURIComponent(
-        JSON.stringify(config),
-      )}; Path=/; HttpOnly; Expires=${expiryDate.toUTCString()}`,
+      `config=${encodeURIComponent(JSON.stringify(config))}; Path=/; HttpOnly; Expires=${expiryDate.toUTCString()}`,
     ]);
-    res
-      .status(200)
-      .json({ success: true, message: 'Registration successsful' });
+    res.status(200).json({ success: true, message: 'Registration successsful' });
   } catch (error) {
     console.error('Error in processing request:', error);
     res.status(500).send('An error occurred while processing your request');
   }
 }
 
-async function getExpoPushToken(
-  deviceId: string,
-  fcmToken: string,
-): Promise<string> {
-  const response = await axios.post(
-    'https://exp.host/--/api/v2/push/getExpoPushToken',
-    {
-      deviceId,
-      experienceId: '@facepunch/RustCompanion',
-      appId: 'com.facepunch.rust.companion',
-      deviceToken: fcmToken,
-      type: 'fcm',
-      development: false,
-    },
-  );
+async function getExpoPushToken(deviceId: string, fcmToken: string): Promise<string> {
+  const response = await axios.post('https://exp.host/--/api/v2/push/getExpoPushToken', {
+    deviceId,
+    experienceId: '@facepunch/RustCompanion',
+    appId: 'com.facepunch.rust.companion',
+    deviceToken: fcmToken,
+    type: 'fcm',
+    development: false,
+  });
   return response.data.data.expoPushToken;
 }
 
-async function registerWithRustPlus(
-  authToken: string,
-  deviceId: string,
-  expoPushToken: string,
-) {
-  const response = await axios.post(
-    'https://companion-rust.facepunch.com/api/push/register',
-    {
-      AuthToken: authToken,
-      DeviceId: deviceId,
-      PushKind: 0,
-      PushToken: expoPushToken,
-    },
-  );
+async function registerWithRustPlus(authToken: string, deviceId: string, expoPushToken: string) {
+  const response = await axios.post('https://companion-rust.facepunch.com/api/push/register', {
+    AuthToken: authToken,
+    DeviceId: deviceId,
+    PushKind: 0,
+    PushToken: expoPushToken,
+  });
   return response.data;
 }
 
