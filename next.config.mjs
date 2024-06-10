61d0408b26ea73ef7aebb9430c26f39e6af1700f
Trophy198 <spsp8426@naver.com>
2024-06-10 22:37:39 +0900

    feat : add remotePatterns URL for Steam profile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.steamstatic.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
