fb2ddc0b9458e7998714b5f8b1593df02e5b22f6
Trophy198 <spsp8426@naver.com>
2024-09-13 10:23:49 +0900

    refactor: add new version credential app link

--- src/data/documents/fcmCredentials/unorderedItems.tsx
+++ src/data/documents/fcmCredentials/unorderedItems.tsx
@@ -28,7 +28,7 @@ export const getCopyCredentialsInWebStep = () => [
 export const getDownloadAndInstallSteps = () => [
   <ListItem key="download-install-app">
     Download and install the{' '}
-    <a href="https://github.com/alexemanuelol/rustplusplus-credential-application/releases/download/v1.2.0/rustplusplus-1.2.0-win-x64.exe">
+    <a href="https://github.com/alexemanuelol/rustplusplus-Credential-Application/releases/v1.4.0">
       rustplusplus FCM Credential Application
     </a>
   </ListItem>,
