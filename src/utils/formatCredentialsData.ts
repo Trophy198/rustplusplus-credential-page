efa0d8426856b63dafbf5676b57a1cd326d90d9b
Trophy198 <spsp8426@naver.com>
2024-09-13 09:17:19 +0900

    refactor: update credential interface and formatter

--- src/utils/formatCredentialsData.ts
+++ src/utils/formatCredentialsData.ts
@@ -1,24 +1,11 @@
 interface Credentials {
   fcm_credentials: {
-    fcm: {
-      name: string;
-      token: string;
-      web: {
-        endpoint: string;
-        p256dh: string;
-        auth: string;
-      };
-    };
     gcm: {
-      token: string;
       androidId: string;
       securityToken: string;
-      appId: string;
     };
-    keys: {
-      privateKey: string;
-      publicKey: string;
-      authSecret: string;
+    fcm: {
+      token: string;
     };
   };
   steamId: string;
@@ -33,21 +20,11 @@ interface FormattedCredentialsData {
 
 export function formatCredentialsData(credentials: Credentials): FormattedCredentialsData {
   const { fcm_credentials, steamId, expire_date, issued_date } = credentials;
-  const { fcm, gcm, keys } = fcm_credentials;
+  const { gcm, fcm } = fcm_credentials;
   const formattedData =
     `/credentials add ` +
-    `keys_private_key:${keys.privateKey} ` +
-    `keys_public_key:${keys.publicKey} ` +
-    `keys_auth_secret:${keys.authSecret} ` +
-    `fcm_name:${fcm.name} ` +
-    `fcm_token:${fcm.token} ` +
-    `fcm_web_endpoint:${fcm.web.endpoint} ` +
-    `fcm_web_p256dh:${fcm.web.p256dh} ` +
-    `fcm_web_auth:${fcm.web.auth} ` +
-    `gcm_token:${gcm.token} ` +
     `gcm_android_id:${gcm.androidId} ` +
     `gcm_security_token:${gcm.securityToken} ` +
-    `gcm_app_id:${gcm.appId} ` +
     `steam_id:${steamId} ` +
     `issued_date:${issued_date} ` +
     `expire_date:${expire_date} `;
